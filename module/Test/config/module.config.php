5da6f0cd403d50075d7f2422220213a14f29bbb2
vagrant <vagrant@zff1-training>
2018-08-10 17:34:34 +0700

    2018-08-10 DB: added initial project + "Test" module

---
+++ module/Test/config/module.config.php
@@ -0,0 +1,32 @@
+<?php
+namespace Test;
+
+use Zend\Router\Http\Literal;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'test' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/test',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
